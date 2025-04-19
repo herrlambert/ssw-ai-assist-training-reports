e1d9fcd9f3f82c55ee422d982bdf4388d9e01ff8
Matt Lambert <mattlam@uw.edu>
2025-04-18 21:18:33 -0700

    AugmentCode Prompt #8 -- fix error from trying to run original script

--- utils/sql_scripts/03_create_update_names_table.sql
+++ utils/sql_scripts/03_create_update_names_table.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS update_names (
     NewLastName VARCHAR(255) NOT NULL,
     NewIdentifier VARCHAR(50) NULL,
     
-    -- Add indexes for commonly queried fields
+    -- Add indexes with length limits to stay under 1000 bytes
     INDEX idx_new_identifier (NewIdentifier),
-    INDEX idx_new_names (NewFirstName, NewLastName)
+    INDEX idx_new_names (NewFirstName(125), NewLastName(125))
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;

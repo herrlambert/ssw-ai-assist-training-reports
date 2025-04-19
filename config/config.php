a10f22c4c6e2773240cf036fe43eb54383d9d13b
Matt Lambert <mattlam@uw.edu>
2025-04-19 14:58:46 -0700

    Change host in config to show host value using on shared hosting environment

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
 // Database configuration
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'mattlam.ovid.u.washington.edu');
 define('DB_USER', 'ssw_training_app');
 define('DB_PASS', 'change_this_password_in_production');
 define('DB_NAME', 'ssw_training_reports');
